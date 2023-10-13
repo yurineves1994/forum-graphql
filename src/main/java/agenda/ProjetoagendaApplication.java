6deb9b8dc57e35ce72b24402b44ba4bdd6b5f6fe
yuri neves <yurineves1934@gmail.com>
2023-10-13 17:06:56 -0300

    feat: init project

---
+++ src/main/java/agenda/ProjetoagendaApplication.java
@@ -0,0 +1,13 @@
+package agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoagendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoagendaApplication.class, args);
+	}
+
+}
