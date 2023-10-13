422a3f6509da0d6cb2535985c3b1e05e5103de7b
yuri neves <yurineves1934@gmail.com>
2023-10-13 17:08:53 -0300

    feat: create schema graphql

---
+++ src/test/java/agenda/projetoagenda/ProjetoagendaApplicationTests.java
@@ -0,0 +1,13 @@
+package agenda.projetoagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoagendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
